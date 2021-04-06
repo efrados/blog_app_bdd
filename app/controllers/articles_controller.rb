bd466067c90806e5a46027af5e02a03329ae7fd6
Efrain Arreche <efrados@gmail.com>
2021-04-06 18:19:38 -0300

    Created Article Succesfully

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,25 @@
+class ArticlesController < ApplicationController
+
+  def index
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      flash[:success] = "Article has been created"
+      redirect_to articles_path
+    else
+      flash.now[:alert] = "Errors"
+      render 'article#new'
+    end
+  end
+
+  private
+    def article_params
+      params.require(:article).permit(:body, :title)
+    end
+end
